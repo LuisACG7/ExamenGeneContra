9f2030131b0b2bdd89bdd6f2e585b129c68a9076
LuisACG7 <cruz.guerrero.luis1jvesp@gmail.com>
2026-02-19 21:41:37 -0600

    refactor: manejo centralizado de respuestas y errores HTTP

---
+++ src/Core/Response.php
@@ -0,0 +1,12 @@
+<?php
+namespace Core;
+
+class Response {
+
+    public static function json($data, int $status = 200) {
+        http_response_code($status);
+        echo json_encode($data);
+        exit;
+    }
+
+}
